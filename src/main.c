655b866c3e7f6ca082dbb8fa9b992767da515067
Victor <zerasul@gmail.com>
2025-09-22 12:31:30 +0200

    initial commit

---
+++ src/main.c
@@ -0,0 +1,49 @@
+#include "raylib.h"
+
+typedef struct {
+    Vector2 position;
+    Vector2 velocity;
+} Player;
+
+int main(void) {
+    InitWindow(800, 600, "Fisicas basicas: posicion, velocidad e inercia");
+    SetTargetFPS(60);
+
+    Player player = {0};
+    player.position = (Vector2){400, 300};
+    player.velocity = (Vector2){0, 0};
+
+    while (!WindowShouldClose()) {
+        // Delta time (tiempo entre frames)
+        float dt = GetFrameTime();
+
+        // Controles: aplicamos aceleración
+        Vector2 acceleration = {0, 0};
+        if (IsKeyDown(KEY_RIGHT)) acceleration.x += 200;
+        if (IsKeyDown(KEY_LEFT))  acceleration.x -= 200;
+        if (IsKeyDown(KEY_DOWN))  acceleration.y += 200;
+        if (IsKeyDown(KEY_UP))    acceleration.y -= 200;
+
+        // Actualizar velocidad con inercia
+        player.velocity.x += acceleration.x * dt;
+        player.velocity.y += acceleration.y * dt;
+
+        // Simular fricción
+        player.velocity.x *= 0.98f;
+        player.velocity.y *= 0.98f;
+
+        // Actualizar posición
+        player.position.x += player.velocity.x * dt;
+        player.position.y += player.velocity.y * dt;
+
+        // Dibujar
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawCircleV(player.position, 20, BLUE);
+            DrawText("Usa flechas para mover con inercia", 10, 10, 20, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
